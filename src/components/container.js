2f08e4b23240be712196cc648963d95b5c1c8c46
Amr Arafa <amr.arafa@robustastudio.com>
2019-11-12 03:03:01 +0200

    style: the modernist landing page add Container component to be used as a wrapper around the content inside all sections to unify content width across the entire document

---
+++ src/components/container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+/** The Container component is used to wrap the content inside all sections to unify content width across the entire document */
+const Container = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+
+export default Container;
