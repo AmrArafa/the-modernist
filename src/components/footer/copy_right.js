797c167b53dc5c2dd30f1de46763dc17db1e933d
Amr Arafa <amr.arafa@robustastudio.com>
2019-11-14 03:46:58 +0200

    feat: footer add CopyRight component and style it

---
+++ src/components/footer/copy_right.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import Container from '../container';
+
+const StyledContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CopyRightWrapper = styled.div`
+  font-size: 13px;
+  color: #979797;
+  padding: 25px 0;
+  border-top: 1px solid #000;
+  background-color: #2b2b2b;
+
+  p {
+    margin: 0;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+
+    :hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const CopyRight = () => {
+  return (
+    <CopyRightWrapper>
+      <StyledContainer>
+        <p>
+          Copyright &copy; 2013 <a href="#!">Domain Name</a> - All Rights Reserved
+        </p>
+        <p>Template by OS Templates</p>
+      </StyledContainer>
+    </CopyRightWrapper>
+  );
+}
+
+export default CopyRight;
