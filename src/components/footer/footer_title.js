5371cb0377536a0f5e42d5315040745719dec609
Amr Arafa <amr.arafa@robustastudio.com>
2019-11-14 03:36:45 +0200

    style: footer Add StyledLink and FooterTitle styled components to avoid redundant styling

---
+++ src/components/footer/footer_title.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const FooterTitle = styled.h1`
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1;
+  color: #fff;
+  margin: 0 0 35px 0;
+`;
+
+export default FooterTitle;
---
+++ src/components/styled_link.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  color: #76c38f;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export default StyledLink;
